ae7dafbbfd166fa2c558ab6b24d063a124d99f56
ac <ac@Jaxxs-MacBook-Pro.local>
2017-03-13 10:37:59 -0400

    Intermediate2

--- MovieSearch/MovieDetailsViewController.swift
+++ MovieSearch/MovieDetailsViewController.swift
@@ -77,20 +77,23 @@ class MovieDetailsViewController: UIViewController {
         print("Exiting getMovieDetails")
     }
     
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let destVC = segue.destination as? PlotSummaryViewController
+        destVC?.imdbID = self.imdbID
     }
-    */
+ 
 
 }
